d250eb6e5bf8d681c4a4046983e39debff0ad38f
kemenykristof <kemeny.kristof24@gmail.com>
2020-01-29 15:56:10 +0100

    function types

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("this should work now");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
