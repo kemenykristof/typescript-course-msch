32a443a7f8bb4e7f9cb5cbbfaa492d521ff30b4a
kemenykristof <kemeny.kristof24@gmail.com>
2020-01-30 19:48:48 +0100

    union types

--- app.ts
+++ app.ts
@@ -1,24 +1,19 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+type Admin = {
+  name: string;
+  privileges: string[];
+};
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
+// interface ElevatedEmployee extends Employee, Admin {}
 
-printResult(add(5, 12));
+type ElevatedEmployee = Admin & Employee;
 
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date()
+};
